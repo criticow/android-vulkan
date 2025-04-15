18ca222b545ac54c55a820c3427f600bfcf0c252
Iury Ferreira Santana <criticow@outlook.com>
2025-04-15 12:16:25 -0300

    live 002

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,16 +1,26 @@
-#include <vulkan/vulkan.h>
-
 #ifdef __ANDROID__
+#define VK_USE_PLATFORM_ANDROID_KHR
 #include "native_app_glue/android_native_app_glue.h"
+struct GLFWwindow;
 #else
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+struct android_app;
 #endif
 
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
+#include <vulkan/vulkan.h>
+#include <optional>
+#include <set>
+
 #include "logger.hpp"
+
+std::vector<const char*> deviceExtensions = {
+  VK_KHR_SWAPCHAIN_EXTENSION_NAME
+};
 
 std::vector<const char*> validationLayers = {
   "VK_LAYER_KHRONOS_validation"
@@ -22,11 +32,32 @@ bool enableValidationLayers = false;
 bool enableValidationLayers = true;
 #endif
 
-VkInstance instance;
-VkDebugUtilsMessengerEXT debugMessenger;
+struct QueueFamilyIndices
+{
+  std::optional<uint32_t> graphicsFamily;
+  std::optional<uint32_t> presentFamily;
+};
+
+struct VulkanConfig
+{
+  VkInstance instance;
+  VkDebugUtilsMessengerEXT debugMessenger;
+  VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+  VkDevice device;
+  VkQueue graphicsQueue;
+  VkQueue presentQueue;
+  VkSurfaceKHR surface;
+};
+
+VulkanConfig vulkanConfig = {};
+bool running = true;
+bool focused = false;
+
+GLFWwindow *glfwWindow = nullptr;
+android_app *androidApp = nullptr;
 
 VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) {
-  LOG_DEBUG("Validation Layer: {}", pCallbackData->pMessage);
+  LOG_DEBUG("VL: {}", pCallbackData->pMessage);
   return VK_FALSE;
 }
 
@@ -56,12 +87,13 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
 
 void setupDebugMessenger()
 {
+  LOG_DEBUG("Creating debug messenger");
   if (!enableValidationLayers) return;
 
   VkDebugUtilsMessengerCreateInfoEXT createInfo;
   populateDebugMessengerCreateInfo(createInfo);
 
-  if (CreateDebugUtilsMessengerEXT(instance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS)
+  if (CreateDebugUtilsMessengerEXT(vulkanConfig.instance, &createInfo, nullptr, &vulkanConfig.debugMessenger) != VK_SUCCESS)
   {
     LOG_DEBUG("Failed to create debug messenger");
   }
@@ -98,6 +130,7 @@ bool checkValidationLayerSupport()
 
 void createInstance(VkInstance *instance)
 {
+  LOG_DEBUG("Creating vulkan instance");
   VkApplicationInfo applicationInfo = {};
   applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
   applicationInfo.pNext = nullptr;
@@ -155,27 +188,297 @@ void createInstance(VkInstance *instance)
   }
 }
 
+void createSurface()
+{
+  LOG_DEBUG("Creating vulkan surface");
+
+  #ifdef __ANDROID__
+  VkAndroidSurfaceCreateInfoKHR createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_ANDROID_SURFACE_CREATE_INFO_KHR;
+  createInfo.pNext = nullptr;
+  // createInfo.flags;
+  createInfo.window = androidApp->window;
+
+  if(vkCreateAndroidSurfaceKHR(vulkanConfig.instance, &createInfo, nullptr, &vulkanConfig.surface) != VK_SUCCESS)
+  {
+    LOG_DEBUG("Failed to create android vulkan surface");
+  }
+  #else
+  if(glfwCreateWindowSurface(vulkanConfig.instance, glfwWindow, nullptr, &vulkanConfig.surface) != VK_SUCCESS)
+  {
+    LOG_DEBUG("Failed to create desktop vulkan surface");
+  }
+  #endif
+}
+
+QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
+{
+  QueueFamilyIndices indices;
+
+  uint32_t queueFamilyCount;
+  vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+  std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyCount);
+  vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilyProperties.data());
+
+  for(int i = 0; i < queueFamilyProperties.size(); i++)
+  {
+    if(indices.graphicsFamily.has_value() && indices.presentFamily.has_value())
+    {
+      break;
+    }
+
+    VkQueueFamilyProperties queueFamily = queueFamilyProperties[i];
+
+    VkBool32 presentSupport = false;
+    vkGetPhysicalDeviceSurfaceSupportKHR(device, i, vulkanConfig.surface, &presentSupport);
+
+    if(presentSupport)
+    {
+      indices.presentFamily = i;
+    }
+
+    if(queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+    {
+      indices.graphicsFamily = i;
+    }
+  }
+
+  return indices;
+}
+
+bool checkDeviceExtensionsSupporte(VkPhysicalDevice device)
+{
+  uint32_t extensionsCount;
+  vkEnumerateDeviceExtensionProperties(vulkanConfig.physicalDevice, nullptr, &extensionsCount, nullptr);
+
+  std::vector<VkExtensionProperties> extensionProperties(extensionsCount);
+  vkEnumerateDeviceExtensionProperties(vulkanConfig.physicalDevice, nullptr, &extensionsCount, extensionProperties.data());
+
+  std::set<std::string> requiredExtensions(deviceExtensions.begin(), deviceExtensions.end());
+
+  for(auto extension : extensionProperties)
+  {
+    requiredExtensions.erase(extension.extensionName);
+  }
+
+  return requiredExtensions.empty();
+}
+
+bool isDeviceSuitable(VkPhysicalDevice device)
+{
+  QueueFamilyIndices indices = findQueueFamilies(device);
+  bool extensionsSupported = checkDeviceExtensionsSupporte(device);
+  return indices.graphicsFamily.has_value() && indices.presentFamily.has_value();
+}
+
+void pickPhysicalDevice()
+{
+  uint32_t physicalDeviceCount;
+  vkEnumeratePhysicalDevices(vulkanConfig.instance, &physicalDeviceCount, nullptr);
+  std::vector<VkPhysicalDevice> physicalDevices(physicalDeviceCount);
+  vkEnumeratePhysicalDevices(vulkanConfig.instance, &physicalDeviceCount, physicalDevices.data());
+
+  LOG_DEBUG("Found {} devices with vulkan support", physicalDeviceCount);
+
+  for(auto &device : physicalDevices)
+  {
+    if(isDeviceSuitable(device))
+    {
+      vulkanConfig.physicalDevice = device;
+      break;
+    }
+  }
+
+  if(vulkanConfig.physicalDevice == VK_NULL_HANDLE)
+  {
+    LOG_DEBUG("Failed to find a suitable device");
+  }
+}
+
+void createLogicalDevice()
+{
+  QueueFamilyIndices indices = findQueueFamilies(vulkanConfig.physicalDevice);
+
+  std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+  std::set<uint32_t> uniqueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+  float queuePriority = 1.0f;
+  for(auto familyIndex : uniqueFamilies)
+  {
+    VkDeviceQueueCreateInfo queueCreateInfo = {};
+    queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.pNext = nullptr;
+    // queueCreateInfo.flags;
+    queueCreateInfo.queueFamilyIndex = familyIndex;
+    queueCreateInfo.queueCount = 1;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+    queueCreateInfos.push_back(queueCreateInfo);
+  }
+
+
+  VkDeviceCreateInfo deviceCreateInfo = {};
+  deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+  deviceCreateInfo.pNext = nullptr;
+  // deviceCreateInfo.flags;
+  deviceCreateInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+  deviceCreateInfo.pQueueCreateInfos = queueCreateInfos.data();
+  // deviceCreateInfo.enabledLayerCount;
+  // deviceCreateInfo.ppEnabledLayerNames;
+  deviceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+  deviceCreateInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+  VkPhysicalDeviceFeatures deviceFeatures = {};
+  deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
+
+  if(vkCreateDevice(vulkanConfig.physicalDevice, &deviceCreateInfo, nullptr, &vulkanConfig.device) != VK_SUCCESS)
+  {
+    LOG_DEBUG("Failed to create logical device");
+  }
+
+  vkGetDeviceQueue(vulkanConfig.device, indices.graphicsFamily.value(), 0, &vulkanConfig.graphicsQueue);
+  vkGetDeviceQueue(vulkanConfig.device, indices.presentFamily.value(), 0, &vulkanConfig.presentQueue);
+}
+
 void cleanUp()
 {
   if (enableValidationLayers) {
-    DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
+    DestroyDebugUtilsMessengerEXT(vulkanConfig.instance, vulkanConfig.debugMessenger, nullptr);
   }
 
-  vkDestroyInstance(instance, nullptr);
+  vkDestroyDevice(vulkanConfig.device, nullptr);
+  vkDestroySurfaceKHR(vulkanConfig.instance, vulkanConfig.surface, nullptr);
+  vkDestroyInstance(vulkanConfig.instance, nullptr);
+
+  #ifndef __ANDROID__
+  glfwDestroyWindow(glfwWindow);
+  glfwTerminate();
+  #endif
+}
+
+void initVulkan()
+{
+  LOG_DEBUG("Initializing Vulkan");
+  createInstance(&vulkanConfig.instance);
+  setupDebugMessenger();
+  createSurface();
+  pickPhysicalDevice();
+  createLogicalDevice();
+}
+
+void pollEvents()
+{
+  #ifdef __ANDROID__
+  int events = 0;
+  struct android_poll_source *source;
+  int timeout = 0;
+
+  while(ALooper_pollOnce(timeout, nullptr, &events, reinterpret_cast<void**>(&source)) >= 0)
+  {
+    if(source) source->process(androidApp, source);
+  }
+  #else
+  glfwPollEvents();
+  running = !glfwWindowShouldClose(glfwWindow);
+  #endif
+}
+
+void centerGLFWWindow(GLFWwindow *window)
+{
+  #ifndef __ANDROID__
+  // Get monitors
+  int monitorCount = 0;
+  GLFWmonitor **monitors = glfwGetMonitors(&monitorCount);
+  // Get monitor position
+  int monitorX = 0;
+  int monitorY = 0;
+  glfwGetMonitorPos(monitors[1], &monitorX, &monitorY);
+  // Get monitor dimensions
+  const GLFWvidmode *videoMode = glfwGetVideoMode(monitors[1]);
+  // Get window dimensions
+  int windowWidth = 0;
+  int windowHeight = 0;
+  glfwGetWindowSize(window, &windowWidth, &windowHeight);
+  // Get frame dimensions
+  int frameTop = 0;
+  int frameLeft = 0;
+  int frameRight = 0;
+  int frameBottom = 0;
+  glfwGetWindowFrameSize(window, &frameLeft, &frameTop, &frameRight, &frameBottom);
+  // Calculate center position
+  int centerX = monitorX + videoMode->width * 0.5f - windowWidth * 0.5f;
+  int centerY = monitorY + frameTop + videoMode->height * 0.5f - windowHeight * 0.5f;
+  // Center window
+  glfwSetWindowPos(window, centerX, centerY);
+  #endif
+}
+
+void initPlatform(android_app *app, int32_t cmd)
+{
+  #ifdef __ANDROID__
+  switch(cmd)
+  {
+    case APP_CMD_INIT_WINDOW:
+    LOG_DEBUG("Initializing Android platform");
+    LOG_DEBUG("APP_CMD_INIT_WINDOW");
+    if(androidApp->window)
+    {
+      running = true;
+      initVulkan();
+    }
+    break;
+    case APP_CMD_TERM_WINDOW:
+    LOG_DEBUG("APP_CMD_TERM_WINDOW");
+    running = false;
+    break;
+    case APP_CMD_GAINED_FOCUS:
+    LOG_DEBUG("APP_CMD_GAINED_FOCUS");
+    focused = true;
+    break;
+    case APP_CMD_LOST_FOCUS:
+    LOG_DEBUG("APP_CMD_LOST_FOCUS");
+    focused = false;
+    break;
+    case APP_CMD_DESTROY:
+    LOG_DEBUG("APP_CMD_DESTROY");
+    running = false;
+    break;
+  }
+  #else
+  LOG_DEBUG("Initializing Desktop platform");
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+  glfwWindow = glfwCreateWindow(1280, 720, "Vulkan", nullptr, nullptr);
+  centerGLFWWindow(glfwWindow);
+  initVulkan();
+  #endif
+}
+
+void run()
+{
+  #ifdef __ANDROID__
+  androidApp->onAppCmd = initPlatform;
+  #else
+  initPlatform(nullptr, 0);
+  #endif
+
+  while(running)
+  {
+    pollEvents();
+  }
+
+  cleanUp();
 }
 
 #ifdef __ANDROID__
 void android_main(struct android_app *app)
 {
-  createInstance(&instance);
-  setupDebugMessenger();
-  cleanUp();
+  androidApp = app;
+  run();
 }
 #else
 int main()
 {
-  createInstance(&instance);
-  setupDebugMessenger();
-  cleanUp();
+  run();
 }
 #endif
